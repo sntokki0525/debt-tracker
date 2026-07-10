d4813a73b7b899da9194fc25d02da41700e84dcc
sntokki0525 <sntokki0525@gmail.com>
2026-07-10 21:29:31 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+const APP_CONFIG = {
+
+salary:80000,
+
+advance:38000,
+
+bonus:37000,
+
+monthlyCreditPayment:45500,
+
+salaryDay:10,
+
+advanceDay:25,
+
+creditDay:26,
+
+fixedExpenses:20500
+
+};
